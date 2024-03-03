0cf7dfcfd5ea575e89d6e5f66cef65d2b25f5622
Kostek <ksytnik73kwer@gmail.com>
2024-03-03 19:05:09 +0100

    Refactor models.py to include TelegramUser model

--- web-app/rest_api/models.py
+++ web-app/rest_api/models.py
@@ -1,14 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
-# Create your models here.
-class Todo(models.Model):
-    task = models.CharField(max_length=180)
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
-    completed = models.BooleanField(default=False, blank=True)
-    updated = models.DateTimeField(auto_now=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+class TelegramUser(models.Model):
+    user_id = models.IntegerField(verbose_name="ID пользователя", unique=True)
+    first_name = models.CharField(verbose_name="Имя пользователя", max_length=255)
+    username = models.CharField(
+        verbose_name="Username пользователя", max_length=255, null=True, blank=True
+    )
+    last_name = models.CharField(
+        verbose_name="Фамилия пользователя", max_length=255, null=True, blank=True
+    )
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
+    is_admin = models.BooleanField(verbose_name="Администратор", default=False)
+    phone_number = models.CharField(
+        max_length=20, verbose_name="Номер телефона", blank=True, null=True
+    )
 
     def __str__(self):
-        return self.task
+        return f"{self.user.username}-{self.user_id}"
+
+    class Meta:
+        db_table = "users"
+

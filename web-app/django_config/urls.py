b77251b0427eae8599a391852adde98cc1295bbf
Kostek <ksytnik73kwer@gmail.com>
2024-03-03 19:02:35 +0100

    Add API authentication and include API URLs in django_config/urls.py

--- web-app/django_config/urls.py
+++ web-app/django_config/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from scaner.views import index
+from rest_api import urls as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path("api/", include(api_urls)),
     path("", index, name="index"),
 ]

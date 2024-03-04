64d07c6b19b80ec0569ebb6c52ad72f9b0be5676
Kostek <ksytnik73kwer@gmail.com>
2024-03-04 22:01:57 +0100

    Add serializers for user, diameter, width, height, tire, cart, order, and order item models

---
+++ web/rest_api/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from .models import (
+    TelegramUser,
+    Diameter,
+    Width,
+    Height,
+    Tire,
+)
+
+from admin_panel.models import CartItem, Order, OrderItem
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TelegramUser
+        fields = "__all__"
+
+
+class DiameterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diameter
+        fields = "__all__"
+
+
+class WidthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Width
+        fields = "__all__"
+
+
+class HeightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Height
+        fields = "__all__"
+
+
+class TireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tire
+        fields = "__all__"
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"

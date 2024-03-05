a6407b57c1b2bd4686f641524a30450581faa728
Kostek <ksytnik73kwer@gmail.com>
2024-03-05 19:52:06 +0100

    Add order_id field to Order model and fix small bugs

--- web/admin_panel/models.py
+++ web/admin_panel/models.py
@@ -26,6 +26,7 @@ class CartItem(models.Model):
 class Order(models.Model):
     """Заказ"""
 
+    order_id = models.PositiveIntegerField(verbose_name="ID заказа", primary_key=True)
     user = models.ForeignKey(
         TelegramUser, on_delete=models.CASCADE, verbose_name="Пользователь"
     )
@@ -34,27 +35,25 @@ class Order(models.Model):
     is_processed = models.BooleanField(default=False, verbose_name="Обработан")
 
     def __str__(self):
-        return f"Заказ #{self.id} от {self.user}"
-
-    class Meta:
-        verbose_name = "Заказ"
-        verbose_name_plural = "Заказы"
-        db_table = "orders"
-
-
-class OrderItem(models.Model):
-    """Элемент заказа"""
-
-    order = models.ForeignKey(
-        Order, related_name="items", on_delete=models.CASCADE, verbose_name="Заказ"
-    )
-    tire = models.ForeignKey(Tire, on_delete=models.CASCADE, verbose_name="Шина")
-    quantity = models.PositiveIntegerField(verbose_name="Количество")
-
-    def __str__(self):
         return f"{self.tire} (x{self.quantity})"
 
     class Meta:
         verbose_name = "Элемент заказа"
         verbose_name_plural = "Элементы заказа"
         db_table = "orders_elements"
+
+
+class OrderItem(models.Model):
+    """Елемент заказа"""
+
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE, verbose_name="Заказ")
+    tire = models.ForeignKey(Tire, on_delete=models.CASCADE, verbose_name="Шина")
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+
+    def __str__(self):
+        return f"Заказ #{self.id} от {self.user}"
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+        db_table = "orders"
